6760968eb70439ac85a2c138b180d4aeefe6ee5f
Skunk-XCP <skunk-xcp@hotmail.fr>
2024-04-09 11:01:38 +0200

    Update debug mode in phaserConfig.js

--- src/assets/config/phaserConfig.js
+++ src/assets/config/phaserConfig.js
@@ -12,7 +12,7 @@ const phaserConfig = {
       default: "arcade",
       arcade: {
          gravity: { y: 0 },
-         debug: true,
+         debug: false,
       },
    },
    scene: [GameScene],
